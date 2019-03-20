b5187f5af4a6fefedade0f27848aa3129f68c2e1
Young Jin Kim <kim687@usc.edu>
2019-03-19 20:45:48 -0700

    add clients

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 let WebSocket = require('ws');
 
-let wss = new WebSocket.Server({ port:process.env.PORT || 8080 });
+let wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 wss.on('connection', (ws) => {
 	ws.on('message',(message) => {
-		console.log(`${message}`);
+		wss.clients.forEach((client) => {
+			client.send(message)
+		});
+		//console.log(`${message}`);
 	});
-
-	setTimeout(() => {
-		ws.send('hello');
-	}, 2000);
 })
